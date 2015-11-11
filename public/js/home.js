d075044f714a3a2d7ea31726e8339acf45a6965e
Aladdin <alaaden3000@gmail.com>
2015-11-11 21:58:30 +0200

    #1

---
+++ public/js/home.js
@@ -0,0 +1,14 @@
+'use strict';
+
+var app = angular.module('mofed', ['ngRoute']);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: 'home.html',
+    controller: 'HomeCtrl'
+  });
+}]);
+
+app.controller('HomeCtrl', ['$scope', function($scope) {
+
+}]);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'شاشة الدخول' });
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home', { title: 'الرئيسية' });
 });
 
 module.exports = router;
