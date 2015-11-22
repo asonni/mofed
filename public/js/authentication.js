a698283b0b89c992454af97f16c8126843365bcd
Aladdin <alaaden3000@gmail.com>
2015-11-22 18:58:58 +0200

    run register with angularjs

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -24,8 +24,20 @@ app.controller('LoginCtrl', ['$scope', function($scope) {
 
 }]);
 
-app.controller('RegisterCtrl', ['$scope', function($scope) {
-
+app.controller('RegisterCtrl', ['$scope', '$location', '$http', function($scope, $location, $http) {
+  $scope.register = function(){
+    $http.post('/user/register',{
+      'email': $scope.email,
+      'password': $scope.password
+    }).success(function (result){
+      $scope.email='';
+      $scope.password='';
+      $location.path("/");
+      // Notification.success({message: 'تمت اضافة قسم جديد بنجاح', title: 'نجاح'});
+    }).error(function (data, status){
+      console.log(data);
+    });
+  };
 }]);
 
 app.controller('RestoreCtrl', ['$scope', function($scope) {
