58752f48bcfb8562d923196d05cdad3867beb002
Aladdin <alaaden3000@gmail.com>
2015-11-22 19:55:39 +0200

    add angular notification lib

--- public/js/authentication.js
+++ public/js/authentication.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('mofed', ['ngRoute']);
+var app = angular.module('mofed', ['ngRoute', 'ui-notification']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/', {
@@ -16,15 +16,30 @@ app.config(['$routeProvider', function($routeProvider) {
     controller: 'RestoreCtrl'
   })
   .otherwise({
-    reditectTo: 'pages/login.html'
+    reditectTo: 'login.html'
   });
 }]);
+
+// Angular Notification Configuration Start
+app.config(function(NotificationProvider) {
+  NotificationProvider.setOptions({
+    delay: 10000,
+    right:10,
+    startTop: 20,
+    startRight: 10,
+    verticalSpacing: 20,
+    horizontalSpacing: 20,
+    positionX: 'center',
+    positionY: 'top'
+  });
+});
+// Angular Notification Configuration End
 
 app.controller('LoginCtrl', ['$scope', function($scope) {
 
 }]);
 
-app.controller('RegisterCtrl', ['$scope', '$location', '$http', function($scope, $location, $http) {
+app.controller('RegisterCtrl', ['$scope', '$location', '$http', 'Notification', function($scope, $location, $http, Notification) {
   $scope.register = function(){
     $http.post('/user/register',{
       'email': $scope.email,
@@ -33,7 +48,7 @@ app.controller('RegisterCtrl', ['$scope', '$location', '$http', function($scope,
       $scope.email='';
       $scope.password='';
       $location.path("/");
-      // Notification.success({message: 'تمت اضافة قسم جديد بنجاح', title: 'نجاح'});
+      Notification.success({message: 'تم التسجيل بنجاح الرجاء التأكيد علي البريد الالكتروني', title: 'نجاح'});
     }).error(function (data, status){
       console.log(data);
     });
