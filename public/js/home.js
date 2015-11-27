30dae33f51398654992a3af3b32ead5c0e12f5c8
Aladdin <alaaden3000@gmail.com>
2015-11-27 22:03:33 +0200

    add check form

--- public/js/home.js
+++ public/js/home.js
@@ -1,18 +1,26 @@
 'use strict';
 
-var app = angular.module('mofed', ['ngRoute']);
+var app = angular.module('mofed', ['ngRoute', 'ngMessages']);
 
 app.config(['$routeProvider', '$locationProvider' , function($routeProvider, $locationProvider) {
   $routeProvider.when('/', {
     templateUrl: '../home.html',
     controller: 'HomeCtrl'
+  })
+  .when('/check', {
+    templateUrl: '../check.html',
+    controller: 'CheckCtrl'
   });
-  $locationProvider.html5Mode({
-    enabled: true,
-    requireBase: false
-  });
+  // $locationProvider.html5Mode({
+  //   enabled: true,
+  //   requireBase: false
+  // });
 }]);
 
 app.controller('HomeCtrl', ['$scope', function($scope) {
+
+}]);
+
+app.controller('CheckCtrl', ['$scope', function($scope) {
 
 }]);
