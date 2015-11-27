4313637931606c1358eaa772eafaa7861e9db4f2
Aladdin <alaaden3000@gmail.com>
2015-11-27 22:37:15 +0200

    #1

--- public/js/home.js
+++ public/js/home.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module('mofed', ['ngRoute', 'ngMessages']);
+var app = angular.module('mofed', ['ngRoute', 'ngMessages', 'ui-notification']);
 
 app.config(['$routeProvider', '$locationProvider' , function($routeProvider, $locationProvider) {
   $routeProvider.when('/', {
@@ -10,6 +10,10 @@ app.config(['$routeProvider', '$locationProvider' , function($routeProvider, $lo
   .when('/check', {
     templateUrl: '../check.html',
     controller: 'CheckCtrl'
+  })
+  .when('/results', {
+    templateUrl: '../results.html',
+    controller: 'ResultsCtrl'
   });
   // $locationProvider.html5Mode({
   //   enabled: true,
@@ -21,6 +25,30 @@ app.controller('HomeCtrl', ['$scope', function($scope) {
 
 }]);
 
-app.controller('CheckCtrl', ['$scope', function($scope) {
+app.controller('CheckCtrl', ['$scope', '$http', 'Notification', function($scope, $http, Notification) {
+  $scope.check = function(){
+    $http.post('/user/check',{
+      'nid': $scope.nid,
+      'regnum': $scope.regnum,
+      'lawnum': $scope.lawnum
+    }).success(function (result){
+      if (result.check == true) {
+        $scope.nid='';
+        $scope.regnum='';
+        $scope.lawnum='';
+        window.location.replace('/user#/results');
+      } else {
+        $scope.nid='';
+        $scope.regnum='';
+        $scope.lawnum='';
+        Notification.error({message: 'الرجاء التأكد من البيانات المدخلة', title: '<div class="text-right">خطأ</div>'});
+      }
+    }).error(function (data, status){
+      console.log(data);
+    });
+  };
+}]);
+
+app.controller('ResultsCtrl', ['$scope', function($scope) {
 
 }]);
