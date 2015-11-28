f199d5f00addb5d8d70762ff2e96b983fcf0638e
Ahmed Fituri <ahmed.elfituri@gmail.com>
2015-11-28 11:37:21 +0200

    adding configurations

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	'db': 'mofed',
-	'user':'mofed',
-	'password': '102030',
-	'url': '127.0.0.1:27017/mofed',
+	'db': 'heroku_kctk8tt2',
+	'user':'heroku_kctk8tt2',
+	'password': '2015naga2016',
+	'url': 'mongodb://ds059634.mongolab.com:59634/heroku_kctk8tt2',
   'epassword' : '2015naga2016'
 };
