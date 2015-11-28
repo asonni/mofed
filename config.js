5cb2fd0273b66b3c3a91f20b9a198e604df3c41a
Ahmed Fituri <ahmed.elfituri@gmail.com>
2015-11-28 12:16:51 +0200

    configurations

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
 	'db': 'heroku_kctk8tt2',
-	'user':'heroku_kctk8tt2',
+	'user':'mofed',
 	'password': '2015naga2016',
-	'url': 'mongodb://heroku_kctk8tt2:2015naga2016@ds059634.mongolab.com:59634/heroku_kctk8tt2',
+	'url': 'mongodb://mofed:2015naga2016@ds059634.mongolab.com:59634/heroku_kctk8tt2',
   'epassword' : '2015naga2016'
 };
