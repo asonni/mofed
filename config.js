9a41588a8d09cdef4381f5653f285adb45962fba
Ahmed Fituri <ahmed.elfituri@gmail.com>
2015-11-28 12:02:25 +0200

    changing configs

--- config.js
+++ config.js
@@ -2,6 +2,6 @@ module.exports = {
 	'db': 'heroku_kctk8tt2',
 	'user':'heroku_kctk8tt2',
 	'password': '2015naga2016',
-	'url': 'mongodb://ds059634.mongolab.com:59634/heroku_kctk8tt2',
+	'url': 'mongodb://heroku_kctk8tt2:2015naga2016@ds059634.mongolab.com:59634/heroku_kctk8tt2',
   'epassword' : '2015naga2016'
 };
