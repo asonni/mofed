94bb0c5812d42ca7f0301e65b2b7d00fc1cd4f41
Ahmed Fituri <ahmed.elfituri@gmail.com>
2015-11-30 00:04:47 +0200

    fixing config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-	'db': 'heroku_kctk8tt2',
+	'db': 'mofed',
 	'user':'mofed',
-	'password': '2015naga2016',
-	'url': 'mongodb://mofed:2015naga2016@ds059634.mongolab.com:59634/heroku_kctk8tt2',
+	'password': '102030',
+	'url': '127.0.0.1:27017/mofed',
   'epassword' : '2015naga2016'
 };
