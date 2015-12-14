2b56fa45f173ac4c19988df9958a5e3d5236df1d
afituri <ahmed.elfituri@gmail.com>
2015-12-14 20:07:25 +0200

    fixing index

--- models/confirm.js
+++ models/confirm.js
@@ -1,16 +1,18 @@
 var mongoose = require('mongoose');
 var timestamps = require('mongoose-timestamp');
+var uniqueValidator = require('mongoose-unique-validator');
 var Schema = mongoose.Schema;
 
 // set up a mongoose model
 
 var Confirm = new Schema({
-    user: {type: Schema.Types.ObjectId , ref: 'User'},
+    user: {type: Schema.Types.ObjectId , ref: 'User',index: true, unique: true, required: true },
     mofednid: {type: Schema.Types.ObjectId , ref: 'Mofednid'},
     mofedbase: {type: Schema.Types.ObjectId , ref: 'Mofedbase'},
     verified: { type: Number, min: 1, max: 10, default:1 },
     admin: {type: Schema.Types.ObjectId , ref: 'User'}
 });
 Confirm.plugin(timestamps);
+Confirm.plugin(uniqueValidator);
 
 module.exports = mongoose.model('Confirm', Confirm);
---
+++ models/false.js
@@ -0,0 +1,25 @@
+var mongoose = require('mongoose');
+var timestamps = require('mongoose-timestamp');
+var Schema = mongoose.Schema;
+
+// set up a mongoose model
+
+var False = new Schema({
+    name: {type: String, index: true, default: "Unknown False"},
+    password: {type: String, required: true},
+    salt: String,
+    email: {type: String, unique : true, required : true},
+    phone: {type: String, default:"NULL"},
+    lawnum: {type: String, index: true},
+    country: {type: String},
+    nid: {type: String, index: true},
+    regnum: {type: String, index: true},
+    status: Boolean,
+    activated : {type: Boolean, default:false},
+    admin : {type: Boolean, default:false},
+    verified: { type: Number, min: 1, max: 10, default:1 },
+    studentId: { type : Date, default: Date.now }
+});
+False.plugin(timestamps);
+
+module.exports = mongoose.model('False', False);
